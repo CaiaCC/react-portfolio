6ba13ae4b0c67091f10cbc0848804828c4a5eb64
Caia Chuang <caiachuang@gmail.com>
2021-04-13 22:57:38 -0700

    Style Navbar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import { FaBars } from "react-icons/fa";
 import {
     Nav,
     NavbarContainer,
@@ -8,22 +8,43 @@ import {
     NavMenu,
     NavItem,
     NavLinks,
+    NavBtn,
+    NavBtnLink,
 } from "./NavbarElements";
-import Logo from '../../images/logo.png';
+import Logo from "../../images/logo.png";
 
 const Navbar = () => {
     return (
         <>
-          <Nav>
-              <NavbarContainer>
-                  <NavLogo to="/">
-                      <img src={Logo} alt="logo" style={{width:'80px'}}/>
-                  </NavLogo>
-                  
-              </NavbarContainer>
-          </Nav>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">
+                        <img src={Logo} alt="logo" style={{ width: "80px" }} />
+                    </NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">Sign up</NavLinks>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink to="/signin">Sign in</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
         </>
-    )
-}
+    );
+};
 
 export default Navbar;
