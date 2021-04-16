b02697dc86cfa820d8ce652d9bb8dc5480401140
Caia Chuang <caiachuang@gmail.com>
2021-04-16 16:25:07 -0700

    Add links to sidebar and style them.

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Link as LinkS } from 'react-scroll';
+import { Link as LinkR } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 
 export const SidebarContainer = styled.aside`
@@ -12,11 +14,8 @@ export const SidebarContainer = styled.aside`
     top: 0;
     left: 0;
     transition: 0.3 ease-in-out;
-    opacity: ${({ isOpen }) =>
-        isOpen
-            ? "100%" : "0"}; // In styled components, can do if else statement
-    top: ${({ isOpen }) =>
-        isOpen ? "0" : "-100%"}; // where the design of dropdown animation
+    opacity: ${({ isOpen }) => isOpen ? "100%" : "0"}; // In styled components, can do if else statement
+    top: ${({ isOpen }) => isOpen ? "0" : "-100%"}; // where the design of dropdown animation
 `;
 
 export const CloseIcon = styled(FaTimes)`
@@ -28,6 +27,63 @@ export const Icon = styled.div`
     top: 1.2rem;
     right: 1.5rem;
     background: transparent;
+    font-size: 2rem;
     cursor: pointer;
     outline: none;
+`
+
+export const SidebarWrapper = styled.div`
+    color: #fff;
+`
+
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screed and (max-width: 480px) {
+        grid-template-rows: repeat(6, 80px);
+    }
+`
+
+export const SidebarLink = styled(LinkS)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+        color: #01bf71;
+        transition: 0.2s ease-in-out;
+    }
+`
+
+export const SideBtnWrap = styled.div`
+display: flex;
+justify-content: center;
+`
+export const SidebarRoute = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
 `
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SidebarContainer, Icon, CloseIcon } from './SidebarElements';
+import React from "react";
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from "./SidebarElements";
 
 const Sidebar = () => {
     return (
@@ -7,8 +7,19 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">About</SidebarLink>
+                    <SidebarLink to="discover">Discover</SidebarLink>
+                    <SidebarLink to="services">Services</SidebarLink>
+                    <SidebarLink to="signup">Sign Up</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
