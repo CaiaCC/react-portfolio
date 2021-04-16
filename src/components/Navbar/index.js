f09e6c4bdeb5f4119bb9eb9b35f3a4592829c276
Caia Chuang <caiachuang@gmail.com>
2021-04-16 16:30:59 -0700

    Add Home page in the pages folder and add to App.js.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -35,10 +35,10 @@ const Navbar = () => {
                             <NavLinks to="services">Services</NavLinks>
                         </NavItem>
                         <NavItem>
-                            <NavLinks to="signup">Sign up</NavLinks>
+                            <NavLinks to="signup">Sign Up</NavLinks>
                         </NavItem>
                         <NavBtn>
-                            <NavBtnLink to="/signin">Sign in</NavBtnLink>
+                            <NavBtnLink to="/signin">Sign In</NavBtnLink>
                         </NavBtn>
                     </NavMenu>
                 </NavbarContainer>
