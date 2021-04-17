a8c75515f345e637c21c122c0f4dfb02c93ef7d2
Caia Chuang <caiachuang@gmail.com>
2021-04-17 13:34:30 -0700

    Add Footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+} from "./FooterElements.js";
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                            <FooterLink to="/signin">About us</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FooterContainer>
+    );
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
 import Projects from "../components/Projects";
+import Footer from "../components/Footer";
 import {
     homeObjOne,
     homeObjTwo,
@@ -26,6 +27,7 @@ const Home = () => {
             <InfoSection {...homeObjTwo}/>
             <Projects />
             <InfoSection {...homeObjThree}/>
+            <Footer/>
         </>
     );
 };
