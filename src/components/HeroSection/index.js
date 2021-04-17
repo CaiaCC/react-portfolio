5deb0b1abcf1f5cf171607a792a6743ef5ddb3a1
Caia Chuang <caiachuang@gmail.com>
2021-04-16 22:54:58 -0700

    Style button in the HeroSection.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     HeroContainer,
     HeroBg,
@@ -7,13 +7,19 @@ import {
     HeroH1,
     HeroP,
     HeroBtnWrapper,
-    Button,
     ArrowForward,
     ArrowRight
 } from "./HeroElements";
+import {Button} from '../ButtonElement';
 import Video from '../../videos/video.mp4'
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
@@ -30,7 +36,11 @@ const HeroSection = () => {
                 <HeroH1>Caia Chuang</HeroH1>
                 <HeroP>Full Stack Web Developer.</HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup">
+                    <Button
+                        to="signup"
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                    >
                         Get Started
                         {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
