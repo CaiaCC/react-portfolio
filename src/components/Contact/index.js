ac40a355e9c362b00aac8c610bf0b6196423f9d6
Caia Chuang <caiachuang@gmail.com>
2021-04-17 22:56:22 -0700

    Create Contact component.

---
+++ src/components/Contact/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    ContactContainer,
+    ContactWrapper,
+    ContactH2,
+    ContactP,
+    ContactIcons,
+    ContactIconLink,
+} from "./ContactElements";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { GoMail } from "react-icons/go";
+
+const Contact = () => {
+    return (
+        <>
+            <ContactContainer id="contact">
+                <ContactWrapper>
+                    <ContactH2>Let's Connect</ContactH2>
+                    <ContactP>You can connect with me here</ContactP>
+                    <ContactIcons>
+                        <ContactIconLink
+                            href="//www.linkedin.com/in/caiachuang/"
+                            target="_blank"
+                            aria-label="Linkedin"
+                        >
+                            <FaLinkedin />
+                        </ContactIconLink>
+                        <ContactIconLink
+                            href="//github.com/CaiaCC"
+                            target="_blank"
+                            aria-label="Github"
+                        >
+                            <FaGithub />
+                        </ContactIconLink>
+                        <ContactIconLink
+                            href="mailto:caiachuang.gmail.com"
+                            target="_blank"
+                            aria-label="email"
+                        >
+                            <GoMail />
+                        </ContactIconLink>
+                    </ContactIcons>
+                </ContactWrapper>
+            </ContactContainer>
+        </>
+    );
+}
+
+export default Contact;
