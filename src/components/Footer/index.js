1a3304b3c69674943aac445d3def6e5e29749413
Caia Chuang <caiachuang@gmail.com>
2021-04-17 22:56:49 -0700

    Add email icon.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { animateScroll as scroll } from "react-scroll";
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { GoMail } from "react-icons/go";
 import {
     Logo,
     FooterContainer,
@@ -45,6 +46,13 @@ const Footer = () => {
                                 aria-label="Github"
                             >
                                 <FaGithub />
+                            </SocialIconLink>
+                            <SocialIconLink
+                                href="mailto:caiachuang.gmail.com"
+                                target="_blank"
+                                aria-label="email"
+                            >
+                                <GoMail />
                             </SocialIconLink>
                         </SocialIcons>
                     </SocialMediaWrap>
